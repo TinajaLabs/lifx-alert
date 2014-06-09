87a6051e3ed0421db76ef9f62b4ceb9bec1ae37e
chris jefferies <chrisjx@gmail.com>
2014-06-08 19:41:12 -0700

    first commit

---
+++ 80-lifxalert.js
@@ -0,0 +1,76 @@
+/**
+ * Open source 2014 Tinaja Labs
+ *
+ **/
+
+// Sample Node-RED node file
+
+// Require main module
+var RED = require(process.env.NODE_RED_HOME+"/red/red");
+// var util = require("util");
+
+// The main node definition - most things happen in here
+function LifxAlertNode(n) {
+
+    var lifx = require('lifx');
+    lifx.setDebug(false);
+    var lx   = lifx.init();
+    var bulb = lx.bulbs[0];
+
+    // Create a RED node
+    RED.nodes.createNode(this,n);
+
+    // Store local copies of the node configuration (as defined in the .html)
+    this.topic = n.topic;
+
+    var msg = {};
+    msg.topic = this.topic;
+
+    // set up the lifx
+    this.on('input', function(msg) {
+
+        // do something with 'msg'
+
+        var hue = 0xcc15;  // the default (purple)
+        if (msg.payload == "1")
+        {
+            hue = 0x2710;  // yellow - warning
+            console.log("sending warning to Lixf");
+        }
+        else if(msg.payload == "2")
+        {
+            hue = 0x0000;  // red - critical
+            console.log("sending critical to Lixf");
+        }
+        else 
+        {
+            hue = 0x4E20;  // green - all good
+            console.log("sending normal to Lixf");
+        }
+
+        var sat = 0xffff
+        var lum = 0x8000;
+        var wcolor = 0;
+        var fade = 0x0513;
+
+        lx.lightsOn(bulb);
+        lx.lightsColour(hue, sat, lum, wcolor, fade);
+        // lx.lightsColour(hue, sat, lum, wcolor, fade, bulb);
+
+        // send out the message to the rest of the workspace.
+        this.send(msg);
+    });
+
+
+
+    this.on("close", function() {
+        // Called when the node is shutdown - eg on redeploy.
+        // Allows ports to be closed, connections dropped etc.
+        // eg: this.client.disconnect();
+        console.log("Closing Lifx...");
+        lx.close();
+    });
+}
+
+// Register the node by name. This must be called before overriding any of the Node functions.
+RED.nodes.registerType("lifxalert",LifxAlertNode);
--- README.md
+++ README.md
@@ -2,3 +2,42 @@ lifx-alert
 ==========
 
 lifx node-red module to show system status using the lifx led light bulb
+
+
+My first attempt at writing a node-red node.  This one is meant to talk to the wifi based Lifx LED light bulb and turn it green, yellow, or red based on an alert condition.  I plan to use it for my home automation system as an indicator of overall system status.  The code was derived originally from the sample demo template node.
+
+Prerequisites:
+
+    npm install lifx - a module from magicmonkey - https://www.npmjs.org/package/lifx - this does the real work of connecting to the lifx wifi light bulbs.
+
+The initial setup of the lifx wifi was done with the android app.
+
+I added these files (80-lifxalert.html, 80-lifxalert.js) into:
+	
+	nodes/core/io/
+
+Maybe they could go into:
+	
+	nodes/core/hardware/
+
+I'm also not sure about what the numbering scheme refers to.  Perhaps you will need to rename the .html and the .js files.
+
+
+Once the lifx package is installed and the lifxalert files are in place, you should restart the node-red application.  I'm running it on a beaglebone black and I start it from the root of the node-red directory like this:
+
+	node --max-old-space-size=256 red.js -v
+
+When you reload the web page you should see a new purple module in the outputs section called lifxalert.
+
+
+To set up a flow and test this out, I created 3 inject nodes to send a string when clicked:
+
+	Payload: string=0, Name: status normal - 0 
+	Payload: string=1, Name: status warning - 1 
+	Payload: string=2, Name: status critical - 2 
+
+Drag the lifxalert module onto the workspace. It has 2 fields, topic and name.  Give the node a unique name like "Lifx Alerts".
+
+Finally I added a debug node to confirm that the output is generated.  You should see a 0, 1, 2 in the debug window when you click on the Inject node.
+
+I'm new to this, so any tips and tricks would be appreciated.  Thanks.
